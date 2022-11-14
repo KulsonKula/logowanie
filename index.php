3726df9addd89122cdc2b9c9f596ec4ca8a10d35
kuba <kulajakub196@gmail.com>
2022-11-14 21:06:04 +0100

    improvement

--- index.php
+++ index.php
@@ -17,19 +17,23 @@ if (isset($_SESSION['loged']) && ($_SESSION['loged'] == true)) {
 </head>
 
 <body>
-	<div class="log">
+	<div class="login">
+		<h1>Welcome back</h1>
+		<h2>Welcome back! Please enter your details.</h2>
 		<form action="login.php" method="post">
-			Login:<br /> <input type="text" name="login" /> <br />
-			Password:<br /> <input type="password" name="password" /> <br />
-			<input type="submit" calue="log in" />
-		</form>
-
-		<a href="registration.php"> Registration</a> <br />
-		<?php
+			Login:<br /> <input type="text" name="login" placeholder="Enter your login" /> <br />
+			Password:<br /> <input type="password" name="password" placeholder="****" /> <br />
+			<input type="submit" value="Log in" />
+			<br /br>
+			<?php
 		if (isset($_SESSION['blad'])) {
 			echo $_SESSION['blad'];
 		}
 		?>
+		</form>
+
+		<h4> Dont have an account? <a href="registration.php">Sign up </a> <br /br></h4>
+
 	</div>
 
 </body>
