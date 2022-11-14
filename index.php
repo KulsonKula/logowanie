9cb085d0850bfff53bcdfe2b3d1b4df0b9e909b7
kuba <kulajakub196@gmail.com>
2022-11-14 21:52:48 +0100

    x

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['loged']) && ($_SESSION['loged'] == true)) {
 <html>
 
 <head>
-<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
 	<meta charset="UTF-8">
 	<title>...</title>
 </head>
@@ -21,15 +21,15 @@ if (isset($_SESSION['loged']) && ($_SESSION['loged'] == true)) {
 		<h1>Welcome back</h1>
 		<h2>Welcome back! Please enter your details.</h2>
 		<form action="login.php" method="post">
-			Login:<br /> <input type="text" name="login" placeholder="Enter your login" /> <br />
-			Password:<br /> <input type="password" name="password" placeholder="****" /> <br />
-			<input type="submit" value="Log in" />
+			<h3>Login</h3><input class="input_B" type="text" name="login" placeholder="Enter your login" />
+			<h3>Password</h3><input class="input_B" type="password" name="password" placeholder="*********" /><br >
+			<input class="log_button" type="submit" value="Log in" />
 			<br /br>
 			<?php
-		if (isset($_SESSION['blad'])) {
-			echo $_SESSION['blad'];
-		}
-		?>
+			if (isset($_SESSION['blad'])) {
+				echo $_SESSION['blad'];
+			}
+			?>
 		</form>
 
 		<h4> Dont have an account? <a href="registration.php">Sign up </a> <br /br></h4>
