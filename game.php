236da84e87ea4ed360030b70e61c18cafd90bfa5
kuba <kulajakub196@gmail.com>
2022-11-24 18:59:34 +0100

    Dodawanie zrobione

--- game.php
+++ game.php
@@ -22,24 +22,37 @@ $connect = @new mysqli($host, $db_user, $db_password, $db_name);
 <body>
     <div class=container>
         <?php
-        if(array_key_exists('delete',$_POST)){
-            $query = "DELETE FROM habits WHERE habit_id=".$_POST['id'];
-            $result=$connect->query($query);
+        if (array_key_exists('delete', $_POST)) {
+            $query = "DELETE FROM habits WHERE habit_id=" . $_POST['id'];
+            $result = $connect->query($query);
+        }
+        if (array_key_exists('habit_name', $_POST)) {
+            $query = "INSERT INTO habits VALUES('" . $_POST['habit_name']."','".$_SESSION['user_id']."',NULL)";
+            $result = $connect->query($query);
         }
         echo "<h1> Welcome " . $_SESSION['login'] . '! [<a href="logout.php">Log out!</a>]</h1>';
-        $user_id=$_SESSION['user_id'];
-        $query="SELECT * FROM habits WHERE user_id=$user_id";
-        $result=$connect->query($query);
+        $user_id = $_SESSION['user_id'];
+        $query = "SELECT * FROM habits WHERE user_id=$user_id";
+        $result = $connect->query($query);
         echo "<div class='wrapper'>";
-        while($row = $result -> fetch_assoc()){
-        echo "<div class='habit'>". $row['name'];
-        echo "<form method='post' action='game.php'>";
-        echo "<button name='delete' type='submit'>delete</button>";
-        echo "<input name='id'value=".$row['habit_id']." style='display:none;'>" ;
-        echo "</form>";
-        echo "</div>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<div class='habit'>" . $row['name'];
+            echo "<form method='post' action='game.php'>";
+            echo "<button name='delete' type='submit'>delete</button>";
+            echo "<input name='id'value=" . $row['habit_id'] . " style='display:none;'>";
+            echo "</form>";
+            echo "</div>";
         }
-        
+        ?>
+        <div class='habit add'>
+            <form method='post' action='game.php'>
+                <label for="habit_name">Habit</label>
+                <input id="habit_name" name="habit_name" type="text" placeholder="Habit name">
+                <input type="submit" value="Create!"/>
+            </form>
+        </div>
+        <?php
+        echo "</div>";
         ?>
     </div>
 </body>
