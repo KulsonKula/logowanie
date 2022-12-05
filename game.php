f5e7db948f2146b98a5851e6af20452aab764799
kuba <kulajakub196@gmail.com>
2022-12-05 10:20:36 +0100

    .

--- game.php
+++ game.php
@@ -34,27 +34,25 @@ $connect = @new mysqli($host, $db_user, $db_password, $db_name);
             echo "<input name='id'value=" . $row['habit_id'] . " style='display:none;'>";
             echo "</form>";
             $habit_id = $row['habit_id'];
-            $query = "SELECT * FROM info WHERE habit_id = $habit_id AND info.date='".date('Y-m-d')."'";
+            $query = "SELECT * FROM info WHERE habit_id = $habit_id AND info.date='" . date('Y-m-d') . "'";
             $row2 = $connect->query($query);
             $data = $row2->fetch_assoc();
-            if($row2->num_rows==0){
-                $query = "INSERT INTO info VALUES(".$row['habit_id'].",'".date('Y-m-d')."',0)";
+            if ($row2->num_rows == 0) {
+                $query = "INSERT INTO info VALUES(" . $row['habit_id'] . ",'" . date('Y-m-d') . "',0)";
                 $connect->query($query);
                 $checked = "";
-            }
-            else{
+            } else {
 
-                if(@$data['date']==date("Y-m-d")&&$data['done']==1)
-                $checked = "checked";
-                else if(@$data['date']==date("Y-m-d")&&$data['done']==0)
-                $checked = "";
+                if (@$data['date'] == date("Y-m-d") && $data['done'] == 1)
+                    $checked = "checked";
+                else if (@$data['date'] == date("Y-m-d") && $data['done'] == 0)
+                    $checked = "";
             }
-                
-            
+
             echo "<form action='changing.php' method='GET'>";
             echo "<input type='checkbox' $checked name='checkbox[]' onchange='this.form.submit()' value=$checked>";
             echo "<input type='text' style='display:none' name='habit_id' value='$habit_id'>";
-            echo "<input type='text' style='display:none' name='date' value='".date("Y-m-d")."'>";
+            echo "<input type='text' style='display:none' name='date' value='" . date("Y-m-d") . "'>";
             echo "</form>";
             echo "</div>";
         }
@@ -63,7 +61,7 @@ $connect = @new mysqli($host, $db_user, $db_password, $db_name);
             <form method='GET' action='adding.php'>
                 <label for="habit_name">Habit</label>
                 <input id="habit_name" name="habit_name" type="text" placeholder="Habit name">
-                <input type="submit" value="Create!"/>
+                <input type="submit" value="Create!" />
             </form>
         </div>
         <?php
