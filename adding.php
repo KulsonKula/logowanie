057941137b96de472cc3396147d09e36255dc599
kuba <kulajakub196@gmail.com>
2022-12-10 17:31:13 +0100

    bigfix

--- adding.php
+++ adding.php
@@ -5,7 +5,7 @@ require_once "connect.php";
 $connect = @new mysqli($host, $db_user, $db_password, $db_name);
 
 
-if (array_key_exists('habit_name', $_GET)) {
+if (!empty($_GET['habit_name'])) {
     $query = "INSERT INTO habits VALUES('" . $_GET['habit_name']."','".$_SESSION['user_id']."',NULL)";
     $result = $connect->query($query);
 } 
